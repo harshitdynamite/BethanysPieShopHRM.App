06733ff34013b186ebee6276b63e39189387f423
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2024-07-28 03:35:23 +0530

    Adding input compnent: An edit form

---
+++ BethanysPieShopHRM.App/Pages/EmployeeEdit.razor.cs
@@ -0,0 +1,23 @@
+﻿using BethanysPieShopHRM.App.Services;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    public partial class EmployeeEdit: ComponentBase
+    {
+        [Inject]
+        public IEmployeeDataService employeeDataService { get; set; }
+
+        [Parameter]
+        public string? EmployeeId { get; set; }
+
+        public Employee Employee { get; set; } = new Employee();
+
+        protected async override Task OnInitializedAsync()
+        {
+            Employee = await employeeDataService.GetEmployeeDetails(int.Parse(EmployeeId));
+        }
+
+    }
+}
