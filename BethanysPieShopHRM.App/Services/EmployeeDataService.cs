43a02e0499982ab41abb40a8847fb1d5112a35fa
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2024-07-28 04:44:58 +0530

    Adding POST/PUT/Delete into the employeeDataService

--- BethanysPieShopHRM.App/Services/EmployeeDataService.cs
+++ BethanysPieShopHRM.App/Services/EmployeeDataService.cs
@@ -1,6 +1,7 @@
 ﻿using BethanysPieShopHRM.App.Helper;
 using BethanysPieShopHRM.Shared.Domain;
 using Blazored.LocalStorage;
+using System.Text;
 using System.Text.Json;
 
 namespace BethanysPieShopHRM.App.Services
@@ -15,14 +16,20 @@ namespace BethanysPieShopHRM.App.Services
             _httpClient = httpClient;
             _localStorageService = localStorageService;
         }
-        public Task<Employee> AddEmployee(Employee employee)
+        public async Task<Employee> AddEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson = new StringContent(JsonSerializer.Serialize(employee),Encoding.UTF8,"application/json");
+            var response = await _httpClient.PostAsync("api/employee", employeeJson);
+            if (response.IsSuccessStatusCode)
+            {
+                return await JsonSerializer.DeserializeAsync<Employee>(await response.Content.ReadAsStreamAsync());
+            }
+            return null;
         }
 
-        public Task DeleteEmployee(int employeeId)
+        public async Task DeleteEmployee(int employeeId)
         {
-            throw new NotImplementedException();
+            await _httpClient.DeleteAsync($"api/employee/{employeeId}");
         }
 
         public async Task<IEnumerable<Employee>> GetAllEmployees(bool refreshRequired = false)
@@ -59,9 +66,10 @@ namespace BethanysPieShopHRM.App.Services
                 new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
         }
 
-        public Task UpdateEmployee(Employee employee)
+        public async Task UpdateEmployee(Employee employee)
         {
-            throw new NotImplementedException();
+            var employeeJson = new StringContent(JsonSerializer.Serialize(employee), Encoding.UTF8, "application/json");
+            await _httpClient.PutAsync("api/employee", employeeJson);
         }
     }
 }
