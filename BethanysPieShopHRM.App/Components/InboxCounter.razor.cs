dcf5fb6012c0b5811466461cc43272b1e2faacd7
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2024-07-28 02:25:35 +0530

    Sharing data across 2 components using ApplicationState

---
+++ BethanysPieShopHRM.App/Components/InboxCounter.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class InboxCounter: ComponentBase
+    {
+        [Inject]
+        public ApplicationState? ApplicationState { get; set; }
+        public int MessageCounter { get; set; } = 0;
+
+        protected override void OnInitialized()
+        {
+            MessageCounter = new Random().Next(10);
+            ApplicationState.NoOfMessages = MessageCounter;
+        }
+
+    }
+}
