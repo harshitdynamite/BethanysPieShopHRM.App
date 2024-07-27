3a49296f57d88a0ed3846881741a9ac88aecb8d6
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2024-07-28 02:04:14 +0530

    Registering depencies into the app using built in DI container

--- BethanysPieShopHRM.App/Program.cs
+++ BethanysPieShopHRM.App/Program.cs
@@ -1,11 +1,13 @@
 using BethanysPieShopHRM.App;
+using BethanysPieShopHRM.App.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+//Registering the dependency using the DI container here
+builder.Services.AddHttpClient<IEmployeeDataService,EmployeeDataService>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
