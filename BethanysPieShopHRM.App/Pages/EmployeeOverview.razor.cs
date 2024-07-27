b275a6ba5f2efa073fa435a741b9239e0ac84699
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2024-07-27 15:28:10 +0530

    Add project files.

---
+++ BethanysPieShopHRM.App/Pages/EmployeeOverview.razor.cs
@@ -0,0 +1,20 @@
+﻿using BethanysPieShopHRM.App.Models;
+using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Pages
+{
+    /// <summary>
+    /// Component class for EmployeeOverview component
+    /// </summary>
+    public partial class EmployeeOverview: ComponentBase
+    {
+        public List<Employee>? Employees { get; set; } = default!;
+
+        protected override void OnInitialized()
+        {
+            Employees = MockDataService.Employees;
+        }
+
+    }
+}
