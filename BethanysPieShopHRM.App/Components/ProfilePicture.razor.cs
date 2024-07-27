1d1f3490b9152c2534b16f248ce7ebb67826b5cb
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2024-07-27 18:58:24 +0530

    Using render fragment + Showing ChildContent usage

---
+++ BethanysPieShopHRM.App/Components/ProfilePicture.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class ProfilePicture
+    {
+        /// <summary>
+        /// the name for this property should be ChildContent
+        /// and type should be RenderFragment
+        /// and it will wired up automatically
+        /// </summary>
+        [Parameter]
+        public RenderFragment? ChildContent { get; set; }
+        //RenderFragment is a built in type tthat represents a segment of UI 
+    }
+}
