915b3fc2a024f98b5e2a702c841168e6a8ee6e1e
Harshit Agarwal <Harshit.Agarwal@landisgyr.com>
2024-07-28 13:56:55 +0530

    Adding Map RazorComponent + Model class for Long/Lat Marker

---
+++ BethanysPieShopHRM.App/Components/Map.razor.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShopHRM.Shared.Model;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class Map
+    {
+        string elementId = $"map-{Guid.NewGuid():D}";
+
+        [Parameter]
+        public double Zoom { get; set; }
+
+        [Parameter]
+        public List<Marker> Markers { get;set; }
+    }
+}
